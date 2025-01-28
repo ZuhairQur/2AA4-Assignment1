66533fd454ed60e2c0abb7751ab29ce6212edf75
ZuhairQur <quresz23@mcmaster.ca>
2025-01-27 23:56:40 -0500

    closes #19; added abstract class for Maze solving algorithm and extended for the particular algorithm, the Right Hand Algorithm. This will be useful in later assignments where different algorithms may be needed.

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolvingAlgorithm.java
@@ -0,0 +1,5 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public abstract class MazeSolvingAlgorithm {
+    public abstract String solveMaze(FreeWalker walker, Maze maze);
+}
---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgorithm.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+public class RightHandAlgorithm extends MazeSolvingAlgorithm {
+    
+/**
+ * Solves the maze using the right-hand rule algorithm.
+ * The walker attempts to navigate the maze by keeping its right hand
+ * in contact with the wall at all times.
+ *
+ * @param walker the FreeWalker object that navigates the maze
+ * @param maze the Maze object representing the maze structure
+ * @return a string of instructions that represents the path taken
+ */
+
+    @Override
+    public String solveMaze(FreeWalker walker, Maze maze) {
+        return null;
+    }
+}
